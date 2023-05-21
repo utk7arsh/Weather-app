cd33e836cc80a7bba1f441a41874bf1bf49541b1
utk7arsh <utkarshlal@gmail.com>
2023-05-20 18:31:10 -0700

    first commit - basic implementation of req, res

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,102 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Head from 'next/head';
+import { useState } from 'react';
+import styled from 'styled-components';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+const InputWrapper = styled.div`
+  margin: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 50px;
+`;
 
-export default IndexPage
+const WeatherCard = styled.div`
+  margin: 10px;
+`;
+
+const TitleWrapper = styled.div`
+  background-color: #1e6cff;
+  padding: 30px;
+`;
+
+const Title = styled.h1`
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  font-family: 'Poppins', sans-serif;
+`;
+
+const HomePage = () => {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState('');
+
+  const fetchWeather = async (e) => {
+    e.preventDefault();
+    const url = `https://api.weatherbit.io/v2.0/current?city=${city}&key=02525ccb316248bba62eb2e25911b795`;
+    try {
+      const res = await fetch(url);
+      const weatherData = await res.json();
+      if (!weatherData.data || weatherData.data.length === 0) {
+        throw new Error('Failed to fetch weather data');
+      }
+      setWeather(weatherData.data[0]); 
+      setError('');
+    } catch (err) {
+      setWeather(null);
+      setError(err.message);
+    }
+  };
+
+  return (
+    <>
+    <div>
+      <TitleWrapper>
+        <Title>Weather App</Title>
+      </TitleWrapper>
+      <InputWrapper>
+        <form onSubmit={fetchWeather}>
+          <input
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            placeholder="Enter city name..."
+            
+          />
+          <button type="submit">Get Weather</button>
+        </form>
+      </InputWrapper>
+      {error && <p>{error}</p>}
+      {weather && (
+        <WeatherCard>
+          <h2>{weather.city_name}, {weather.country_code}</h2>
+          <p>Temperature: {weather.temp}°C</p>
+          <p>Humidity: {weather.rh}%</p>
+          <p>Description: {weather.weather.description}</p>
+        </WeatherCard>
+      )}
+    </div>
+    </>
+  );
+};
+
+export default HomePage;
+
+
+
+
+// import Link from 'next/link'
+// import Layout from '../components/Layout'
+
+// const IndexPage = () => (
+//   <Layout title="Home | Next.js + TypeScript Example">
+//     <h1>Hello Next.js 👋</h1>
+//     <p>
+//       <Link href="/about">About</Link>
+//     </p>
+//   </Layout>
+// )
+
+// export default IndexPage
